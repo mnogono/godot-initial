8070388dc7a25fd93dae9db265de120feda656c7
mnogo <mnogono@gmail.com>
2025-12-27 21:51:45 +0500

    wip skelet_1 attack animation

--- Scenes/Enemies/Skeleton1/skeleton_1.gd
+++ Scenes/Enemies/Skeleton1/skeleton_1.gd
@@ -1,61 +1,125 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
-@onready var view_collision = $View/CollisionShape2D
+# view area collision, player intersect this activate walk to player
+@onready var view_collision = $ViewArea/CollisionShape2D
 
 @export var speed = 15
-var direction: float
+@export var speed_chase = 25
+
+var move_direction: float
 var view_direction: int
 var player_in_vision: bool
+var player_in_attack_range: bool
+var is_attacking: bool
+var state: State
+enum State {Idle, Walk, Attack, Chase}
 
 func _ready() -> void:
-	direction = 0
+	state = State.Idle
+	move_direction = 0
 	view_direction = 1
-	view_collision.position.x = 75
+	view_collision.position.x = view_collision.shape.size.x / 2
 	player_in_vision = false
+	player_in_attack_range = false
+	is_attacking = false
+	#anim.animation_finished.connect(_on_attack_animation_finished)
 
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	velocity.x = direction * speed
-	if velocity.x < 0:
+func _play_animation(animation: String) -> void:
+	if anim.animation == animation:
+		return
+	anim.play(animation)
+
+func state_animation() -> void:
+	match state:
+		State.Idle: _play_animation("idle")
+		State.Walk: _play_animation("walk")
+		State.Chase: _play_animation("walk")
+		State.Attack: _play_animation("attack")
+		
+func state_physics_process() -> void:
+	match state:
+		State.Walk:
+			velocity.x = move_direction * speed
+		State.Chase:
+			velocity.x = move_direction * speed_chase
+		State.Attack, State.Idle:
+			velocity.x = 0
+	if velocity.x < 0 || view_direction == -1:
 		anim.flip_h = true
 	else:
 		anim.flip_h = false
 	
+		
+func state_update() -> void:
+	if is_attacking:
+		state = State.Attack
+	elif player_in_attack_range:
+		state = State.Attack
+	elif player_in_vision:
+		state = State.Chase
+	else:
+		state = State.Idle
+		
+
+func update_view_collision_position() -> void:
 	if view_direction == 1:
-		view_collision.position.x = 75
+		view_collision.position.x = view_collision.shape.size.x / 2
 	else:
-		view_collision.position.x = -75
+		view_collision.position.x = -view_collision.shape.size.x / 2
 
-	if player_in_vision == false:
-		velocity.x = 0
-
+func _physics_process(delta: float) -> void:
+	state_physics_process()
+	state_animation()
 	move_and_slide()
-	
-	#if velocity.x == 0:
-	#	direction = direction * -1
-	
-	if velocity.x != 0:
-		anim.play("walk")
-	else:
-		anim.play("idle")
 
+
+func wait(sec: float) -> void:
+	await get_tree().create_timer(sec).timeout
 
 func _on_view_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_in_vision = true
-		if body.global_position.x > global_position.x:
-			direction = 1
-		elif body.global_position.x < global_position.x:
-			direction = -1
+		await wait(1.0)
+		move_direction = sign(body.global_position.x - global_position.x)
+		state_update()
 
 
 func _on_view_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_in_vision = false
+		move_direction = 0
+		state_update()
 
 
 func _on_timer_timeout() -> void:
 	if player_in_vision == false:
 		view_direction = -1 * view_direction
+		update_view_collision_position()
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_attack_range = true
+		state_update()
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_attack_range = false
+		state_update()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.animation == "attack":
+		is_attacking = false
+		state_update()
+		print("animation finished: " + anim.animation)
+
+
+func _on_animated_sprite_2d_animation_changed() -> void:
+	if anim == null:
+		return
+	if anim.animation == "attack":
+		is_attacking = true
+		state_update()
